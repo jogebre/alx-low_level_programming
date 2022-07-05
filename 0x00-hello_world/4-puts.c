0c6b41126b07f74a19fa9f718854a2167f1b24ab
jogebre <jhon23gebre@gmail.com>
2022-07-05 14:15:27 +0300

    4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * *main - entry point.
  * *Description: Prints the description specified
  * *Return: always zero(success)
- * */
+ **/
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
