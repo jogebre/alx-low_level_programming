5aeb89881a0b5c08b2f88a28840a4b8a7f74d0dc
jogebre <jhon23gebre@gmail.com>
2022-07-05 18:37:27 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet- prints alphabet
+ **/
+void print_alphabet(void)
+{
+	char atoz;
+
+	for (atoz = 'a'; atoz <= 'z'; atoz++)
+
+	{
+		_putchar(atoz);
+	}
+	_putchar('\n')
+}
